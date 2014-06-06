d336b6c4aabbb1a35a077593a5473dac3b8824db
Joshua Howland <development@jkhowland.com>
2014-06-06 09:34:26 -0600

    Initial Commit

---
+++ Time-Tracker/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  Time-Tracker
+//
+//  Created by Joshua Howland on 6/6/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Time-Tracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Time-Tracker
+//
+//  Created by Joshua Howland on 6/6/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
